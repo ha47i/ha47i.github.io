6b085191365a4e49180f505c538a4616cf58ade6
IceLiuli <ocuex@outlook.com>
2025-08-13 04:06:49 +0800

    URL跳转页预览版1.2

---
+++ url/config.js
@@ -0,0 +1,8 @@
+// 全局配置
+const origin_platform = 'gitee'; // 'github' 或 'gitee'
+const origin_apiUrl = 'https://gitee.com/api/v5/repos/hsq79461/url-go/issues/ICSMFQ/comments'; // api接口
+// const origin_apiUrl = 'https://api.github.com/repos/ha47i/url-go/issues/1/comments'; // api接口
+const creator_list = []; // 创建者列表（小写）['hsq79461', 'ha47i'];
+const list_mode = 'black'; // 'white' 或 'black'
+const case_sensitive = false; // 是否区分大小写，默认为 false
+const open_newtab = true; // 是否在新标签页打开URL，默认为 false
